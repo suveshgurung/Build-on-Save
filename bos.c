fd8ec9fc0cac83e3914440453b0a58e7e4ec16d7
suvesh <suveshgurung2@gmail.com>
2024-10-02 10:18:50 +0545

    get last modified time of the file

--- bos.c
+++ bos.c
@@ -4,23 +4,51 @@
 
 
 #include "bos.h"
-#include <pthread.h>
-#include <unistd.h>
+#include <stdio.h>
 
 /* global variables */
 
+time_t oldmTime;     // last modified time of the file.
+#ifdef _WIN32
+DWORD processid;
+#elif __unix__
 pid_t processid;
+#endif
 
 /* function definitions */
 
-void BOS_Init() {
+void BOS_Init(char *sourceFileName) {
+    // Get the process ID.
     #ifdef _WIN32
-        processid = GetCurrentProcessId();
+    processid = GetCurrentProcessId();
     #elif __unix__
-        processid = getpid(); 
+    processid = getpid(); 
     #endif
 
     printf("Current Process ID : %d\n", processid);
+
+    // Get the last modified date of the file.
+    struct stat fileStat;
+    #ifdef _WIN32
+    TCHAR filePathBuf[FILE_PATH_LEN];
+    GetModuleFileName(NULL, filePathBuf, FILE_PATH_LEN);
+    #elif __unix__
+    char filePath[FILE_PATH_LEN] = {'\0'};
+    if (getcwd(filePath, sizeof(filePath)) == NULL) {
+        perror(" [BOS_Init] getcwd");
+        exit(errno);
+    }
+    #endif
+
+    char sourceFilePath[FILE_PATH_LEN] = {'\0'};
+    snprintf(sourceFilePath, FILE_PATH_LEN, "%s/%s", filePath, sourceFileName);
+    printf("%s\n", sourceFilePath);
+    // TODO : See how to get the path of the file.
+    if (stat(sourceFilePath, &fileStat) == -1) {
+        perror(" [BOS_Init] stat");
+        exit(errno);
+    }
+    oldmTime = fileStat.st_mtim.tv_sec;
 
     BOS_Create_Thread();
 }
@@ -29,15 +57,15 @@ void BOS_Create_Thread() {
     pthread_t bosThread; 
 
     if (pthread_create(&bosThread, NULL, BOS_Check_Is_File_Saved, NULL)) {
-        perror("pthread_create");
-        exit(100);
+        perror(" [BOS_Create_Thread] pthread_create");
+        exit(errno);
     }
 }
 
 void *BOS_Check_Is_File_Saved(void *arg) {
-    while (1) {
-        printf("1234\n");
-    }
+    // while (1) {
+    //     printf("1234\n");
+    // }
 
     return NULL;
 }
--- bos.h
+++ bos.h
@@ -5,21 +5,35 @@
 
 #pragma once
 
-/* defines and global variables */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <pthread.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+// #include <fcntl.h>
 #ifdef _WIN32
 #include <windows.h>
-extern DWORD processid;
 #elif __unix__
 #include <unistd.h>
+#endif
+
+/* defines */
+
+#define FILE_PATH_LEN 10000
+
+/* global variables */
+
+extern time_t oldmTime;     // last modified time of the file.
+#ifdef _WIN32
+extern DWORD processid;
+#elif __unix__
 extern pid_t processid;
 #endif
 
 /* function declarations */
 
-void BOS_Init();
+void BOS_Init(char *);
 void BOS_Create_Thread();
 void *BOS_Check_Is_File_Saved(void *);
